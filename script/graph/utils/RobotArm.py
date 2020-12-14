e62611fdf8b61e7e6cb0bf06b4c95f5d38ba867e
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-12-14 23:35:21 +0100

    simplify code

--- script/graph/utils/RobotArm.py
+++ script/graph/utils/RobotArm.py
@@ -1,38 +1,14 @@
-from utils.Point3D import *
+from utils.Zone import *
 
 
-class RobotArm(Point3D):
+class RobotArm(Zone):
     def __init__(self, x, y, z=0, name=" "):
-        super().__init__([x, y, z])
-
-        self.name = "RobotArm - " + name
-
-        self.__child = []
-        self.__parent = None
-        self.size = 150
-
-    def setChild(self, nodeChild):
-        self.__child.append(nodeChild)
-
-    def getChild(self):
-        return self.__child
-
-    def getParent(self):
-        return self.__parent
-
-    def isGoal(self):
-        return False
+        super().__init__("RobotArm - " + name, [x, y, z], 150)
 
     def __str__(self):
         rep = "RobotArm :\n"
 
         rep += "    Pose (x,y,z): ("+str(self.x)+"," + \
             str(self.y)+","+str(self.z)+")\n"
-
-        # if(self.__child):
-        #     for child in self.__child:
-        #         rep += "    child: " + child.name + "\n"
-        # else:
-        #     rep += "    child: " + str(self.__child) + "\n"
 
         return rep
